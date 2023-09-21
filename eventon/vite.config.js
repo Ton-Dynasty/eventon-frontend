4c5b5ef143ca6f2561f5dae8f1a6f589310b81d6
WaterSo0910 <jacky950141@gmail.com>
2023-09-22 04:29:52 +0800

    Update vite.config.js

--- eventon/vite.config.js
+++ eventon/vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/eventon-frontend/',
   plugins: [react()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
